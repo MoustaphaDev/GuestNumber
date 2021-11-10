11b232a1be59c7ed7065a48dde5bb95bacafbfea
MoustaphaDev <81974850+MoustaphaDev@users.noreply.github.com>
2021-11-10 00:43:44 +0000

    fixed: input-type "number" instead of "text"

--- src/components/GuessWidget/GuessInput/GuestInput.tsx
+++ src/components/GuessWidget/GuessInput/GuestInput.tsx
@@ -1,20 +1,20 @@
 const GuestInput = ({
   userNumber,
-  setUserNumber
+  setUserNumber,
 }: {
-  setUserNumber: React.Dispatch<React.SetStateAction<string>>
-  userNumber: string
+  setUserNumber: React.Dispatch<React.SetStateAction<string>>;
+  userNumber: string;
 }) => {
   return (
     <input
       className="py-3 px-4 w-full max-w-sm rounded-full border-2 border-black focus-within:border-blue-600 focus:border-blue-600 focus:shadow-lg transition-shadow ease-in-out focus:outline-none"
-      type="text"
+      type="number"
       onChange={(e) => {
-        setUserNumber(e.target.value)
+        setUserNumber(e.target.value);
       }}
       value={userNumber}
     />
-  )
-}
+  );
+};
 
-export default GuestInput
+export default GuestInput;
